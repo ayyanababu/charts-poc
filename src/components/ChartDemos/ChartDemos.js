68c9e37a6b1e2e43186b71c2b6eb45e73c90718f
Wajid2001 <67428865+Wajid2001@users.noreply.github.com>
2025-02-25 00:55:42 -0600

    Add VSCode settings, update package dependencies, and enhance ChartDemos with new charts

--- src/components/ChartDemos/ChartDemos.js
+++ src/components/ChartDemos/ChartDemos.js
@@ -1,25 +1,23 @@
-import React from 'react'
-import CombinationChart from './HighCharts/CombinedChart'
-import DonutChart from './HighCharts/DonutChart'
+import { DonutChart, GroupedBarChart, ThemeProvider } from '@my-org/ui-library'
+import React, { useEffect, useState } from 'react'
 import "./ChartDemos.css"
-import LargeDataChart from './HighCharts/LargeDataChart'
-import ChartjsCombinationChart from './Chartjs/CombinedChart'
-import ChartjsDonutChart from './Chartjs/DonutChart'
-import ChartjsLargeDataChart from './Chartjs/LargeDataChart'
+import { NivoCombinationChart, NivoDonutChart } from './Nivo/NivoComponents'
+import NivoStackedBarChart from './Nivo/StackedBarChart'
 import RechartsCombinationChart from './Recharts/CombinedChart'
 import RechartsDonutChart from './Recharts/DonutChart'
-import RechartLargeDatasetChart from './Recharts/LargeDataChart'
-import { NivoCombinationChart, NivoDonutChart, NivoLargeDataChart } from './Nivo/NivoComponents'
-import VisxDonutChart from './Visx/VisxDonutChart'
-import VisxCombinationChart from './Visx/VisxCombinedChart'
-import VisxLargeDataChart from './Visx/LargeDataChart'
 import RechartsStackedBarChart from './Recharts/StackedBarChart'
-import VisxStackedBarChart from './Visx/StackedBarChart'
-import NivoStackedBarChart from './Nivo/StackedBarChart'
 
 export const MAX_POINTS = 500;
 
 export default function ChartDemos() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+  }, []);
+
   return (
     <div className='p-2 flex flex-col gap-4'>
       {/* <div className='flex flex-col gap-2'> 
@@ -73,10 +71,143 @@ export default function ChartDemos() {
           Visx
         </h1>
         <div className='flex flex-row gap-2' style={{maxWidth: "100%"}}>
-          <VisxCombinationChart />
-          <VisxDonutChart />
+          {/* <VisxCombinationChart />
+          <VisxDonutChart /> */}
+          
+          <ThemeProvider themeMode="dark">
+            <div style={{ width:"30%", height: 500, border: "1px solid black", padding: 20 }}>
+              <GroupedBarChart
+                width={600}
+                height={200}
+                data={[
+                  {
+                    label: 'Aasdfasdf asd',
+                    data: {
+                      future: 25,
+                      options: 40,
+                      forwards: 15,
+                      fixedIncome: 12,
+                      others: 32,
+                    },
+                  },
+                  {
+                    label: 'Basdfasdf asd',
+                    data: {
+                      future: 30,
+                      options: 1,
+                      forwards: 12,
+                      fixedIncome: 12,
+                      others: 32,
+                    },
+                  },
+                  {
+                    label: 'Casdfasdf asd',
+                    data: {
+                      future: 15,
+                      options: 50,
+                      forwards: 1,
+                      fixedIncome: 12,
+                      others: 32,
+                    },
+                  },
+                  {
+                    label: 'Dasdfasdf asd',
+                    data: {
+                      future: 40,
+                      options: 35,
+                      forwards: 10,
+                      fixedIncome: 12,
+                      others: 32,
+                    },
+                  },
+                ]}
+                groupKeys={[
+                  'future',
+                  'options',
+                  'forwards',
+                  'fixedIncome',
+                  'others',
+                ]}
+                timestamp={new Date().toISOString()}
+                isLoading={isLoading}
+                title="Grouped Bar Chart"
+              />
+            </div>
+            <div style={{ width:"30%", height: 500, border: "1px solid black", padding: 20 }}>
+              <DonutChart
+                data={[
+                  { label: 'Scheduled', value: 60 },
+                  { label: 'Completed', value: 15 },
+                  { label: 'Seat', value: 25 },
+                ]}
+                timestamp={new Date().toISOString()}
+                isLoading={isLoading}
+                title="Donut Chart"
+                />
+            </div>
+            
+            <div style={{ width: "30%", height: 500, border: "1px solid black", padding: 20 }}>
+              <GroupedBarChart
+                width={600}
+                height={200}
+                type="stacked"
+                data={[
+                  {
+                    label: 'Aasdfasdf asd',
+                    data: {
+                      future: 25,
+                      options: 40,
+                      forwards: 15,
+                      fixedIncome: 12,
+                      others: 32,
+                    },
+                  },
+                  {
+                    label: 'Basdfasdf asd',
+                    data: {
+                      future: 30,
+                      options: 1,
+                      forwards: 12,
+                      fixedIncome: 12,
+                      others: 32,
+                    },
+                  },
+                  {
+                    label: 'Casdfasdf asd',
+                    data: {
+                      future: 15,
+                      options: 50,
+                      forwards: 1,
+                      fixedIncome: 12,
+                      others: 32,
+                    },
+                  },
+                  {
+                    label: 'Dasdfasdf asd',
+                    data: {
+                      future: 40,
+                      options: 35,
+                      forwards: 10,
+                      fixedIncome: 12,
+                      others: 32,
+                    },
+                  },
+                ]}
+                groupKeys={[
+                  'future',
+                  'options',
+                  'forwards',
+                  'fixedIncome',
+                  'others',
+                ]}
+                timestamp={new Date().toISOString()}
+                isLoading={isLoading}
+                title="Grouped Bar Chart"
+              />
+            </div>
+          </ThemeProvider>
           {/* <VisxLargeDataChart /> */}
-          <VisxStackedBarChart />
+          {/* <VisxStackedBarChart /> */}
         </div>
       </div>
     </div>
